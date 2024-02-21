663d510b16ce34bc843b46ed1608377d5331965c
Andrej SH <Andrej.sh2017@mail.ru>
2024-02-21 22:10:39 +0300

    Добавьте файлы проекта.

---
+++ Kontr_2_2/Kontr_2_2.cpp
@@ -0,0 +1,100 @@
+﻿// Kontr_2_2.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+
+
+#include <iostream>
+#include <string>
+#include <stdio.h>
+#include <stdlib.h>
+#include <windows.h>
+
+
+using namespace std;
+
+int main()
+{
+	SetConsoleCP(1251);
+	string str;
+	string control;
+	cout << "Enter SNILS(9*n)" << endl;
+	cin >> str;
+	//cout << str.length() << endl;
+
+	int cont = 0;
+
+	int summ101, num9, num8, num7, num6, num5, num4, num3, num2, num1;
+	int summ;
+	string str1, str2, str3, str4, str5, str6, str7, str8, str9;
+
+	str1 = str[0];
+	str2 = str[1];
+	str3 = str[2];
+	str4 = str[3];
+	str5 = str[4];
+	str6 = str[5];
+	str7 = str[6];
+	str8 = str[7];
+	str9 = str[8];
+
+
+	num9 = stoi(str1);
+	//cout << num9 << endl;
+	num8 = stoi(str2);
+	//cout << num8 << endl;
+	num7 = stoi(str3);
+	//cout << num7 << endl;
+	num6 = stoi(str4);
+	//cout << num6 << endl;
+	num5 = stoi(str5);
+	//cout << num5 << endl;
+	num4 = stoi(str6);
+	//cout << num4 << endl;
+	num3 = stoi(str7);
+	//cout << num3 << endl;
+	num2 = stoi(str8);
+	//cout << num2 << endl;
+	num1 = stoi(str9);;
+	//cout << num1 << endl;
+	summ = (num9 * 9) + (num8 * 8) + (num7 * 7) + (num6 * 6) + (num5 * 5) + (num4 * 4) + (num5 * 5) + (num3 * 3) + (num2 * 2) + (num1 * 1);
+
+	//cout << summ << endl;
+
+	if (num9 == num8 || num8 == num7 || num7 == num6 || num6 == num5 || num5 == num4 || num4 == num3 || num3 == num2 || num2 == num1) {
+		cont = 0;
+		cout << "\nContr number is Not walid(||cont = 0) - " << cont << endl;
+	}
+	else if (summ > 101) {
+		cout.precision(2);
+		summ101 = summ % 101;
+		//cout << summ101 << endl;
+		if (summ101 < 100) {
+			cont = summ101;
+			cout << "\nContr number is walid(cont = summ101) - " << cont << endl;
+		}
+		else if (summ101 == 100) {
+			cont = 0;
+			cout << "\nContr number is NOT walid(cont = 0) - " << cont << endl;
+		}
+	}
+	else if (summ < 100) {
+		cont = summ;
+		cout << "\nContr number is walid(cont = summ) - " << cont << endl;
+	}
+	else if (summ == 100 || summ == 101) {
+		cont = 0;
+		cout << "\nContr number is walid(cont = 0) - " << cont << endl;
+	}
+
+}
+
+// Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
+// Отладка программы: F5 или меню "Отладка" > "Запустить отладку"
+
+// Советы по началу работы 
+//   1. В окне обозревателя решений можно добавлять файлы и управлять ими.
+//   2. В окне Team Explorer можно подключиться к системе управления версиями.
+//   3. В окне "Выходные данные" можно просматривать выходные данные сборки и другие сообщения.
+//   4. В окне "Список ошибок" можно просматривать ошибки.
+//   5. Последовательно выберите пункты меню "Проект" > "Добавить новый элемент", чтобы создать файлы кода, или "Проект" > "Добавить существующий элемент", чтобы добавить в проект существующие файлы кода.
+//   6. Чтобы снова открыть этот проект позже, выберите пункты меню "Файл" > "Открыть" > "Проект" и выберите SLN-файл.
---
+++ Kontr_2_4/Kontr_2_4.cpp
@@ -0,0 +1,131 @@
+﻿// Kontr_2_4.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+#include <windows.h>
+#include <math.h> 
+#include <cmath>
+#include <clocale>
+#include <random>
+#include <cstdlib>
+
+using namespace std;
+
+int main()
+{
+    setlocale(LC_CTYPE, "rus");
+    //SetConsoleCP(1251);
+    wcout << L"Центр мишени случайно распологается в пределах координат от 0 до 100 \n Расчёт очков: \n +/-10 от центра мишени = 10, \n  +/-20 от центра мишени = 5, \n   +/-30 от центра мишени = 1, \n       больше чем +/-30 от центра мишени = 0  " << endl;
+
+    srand(time(0));
+    int O_x = rand() % 100;
+    wcout << O_x << endl;
+
+    int start_y = 0;
+    int end_y = 100;
+    int O_y = rand() % 100;
+    wcout << O_y << endl;
+
+    int n = 0; // минимальное число очков для окончания стрельбы
+    wcout << L"Необходимо ввести минимальное чимло очков для завершения стрельбы: ";
+
+    wcin >> n;
+
+    int i1_0 = 0;
+    int i;
+    int i0;
+    int x_shot;
+    int y_shot;
+    int radius_mishen;
+    int shots = 0;
+    double radius_shot_ot_mishen;
+
+
+
+
+
+    for (i = 1; i1_0 <= n; i++) {
+
+        wcout << L"Произведите выстрел № " << i << endl;
+
+        //Координаты выстрела по шкале х
+        wcout << L"-введите координату х: ";
+
+
+        wcin >> x_shot;
+
+        //Координаты выстрела по шкале у
+        wcout << L"-введите координату y: ";
+
+        wcin >> y_shot;
+
+        radius_mishen = pow(abs((O_x - x_shot)), 2) + pow(abs((O_y - y_shot)), 2);
+        radius_shot_ot_mishen = sqrt(radius_mishen);
+        wcout << radius_shot_ot_mishen << endl;
+        //Условия определения очков от попадания
+
+        int razryd = 0;
+
+
+        if (radius_shot_ot_mishen >= -30 && radius_shot_ot_mishen <= 30)
+        {
+            razryd = 1;
+        }
+        else
+        {
+            razryd = 0;
+        }
+
+        if (radius_shot_ot_mishen >= -20 && radius_shot_ot_mishen <= 20)
+        {
+            razryd = 5;
+        }
+        if (radius_shot_ot_mishen >= -10 && radius_shot_ot_mishen <= 10)
+        {
+            razryd = 10;
+        }
+
+        i1_0 = razryd + i1_0;
+
+
+        wcout << L"суммарный результат после выстрелов в количестве:" << i;
+        wcout << " = " << i1_0 << endl;
+        shots++;
+        i0 = i1_0;
+    }
+
+
+
+
+
+    wcout << L"Число сделанных выстрелов: " << shots << endl;
+    double master = i0 / shots;
+    wcout << master << endl;
+    // wcout << i0 << endl;
+    if (master == 10) {
+        wcout << L"Это соответствует уровню СНАЙПЕР" << endl;
+    }
+    else if (master <= 8.4 and master >= 3.6) {
+        wcout << L"Это соответствует уровню СТРЕЛОК" << endl;
+    }
+    else {
+        wcout << L"Это соответствует уровню НОВИЧЁК" << endl;
+    }
+
+
+
+
+    //cin.get();
+}
+
+
+// Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
+// Отладка программы: F5 или меню "Отладка" > "Запустить отладку"
+
+// Советы по началу работы 
+//   1. В окне обозревателя решений можно добавлять файлы и управлять ими.
+//   2. В окне Team Explorer можно подключиться к системе управления версиями.
+//   3. В окне "Выходные данные" можно просматривать выходные данные сборки и другие сообщения.
+//   4. В окне "Список ошибок" можно просматривать ошибки.
+//   5. Последовательно выберите пункты меню "Проект" > "Добавить новый элемент", чтобы создать файлы кода, или "Проект" > "Добавить существующий элемент", чтобы добавить в проект существующие файлы кода.
+//   6. Чтобы снова открыть этот проект позже, выберите пункты меню "Файл" > "Открыть" > "Проект" и выберите SLN-файл.
